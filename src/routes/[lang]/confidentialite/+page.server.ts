c247163510f4d41b88f204e92a1463c00e341e84
ebauger <etienne.bauger@gmail.com>
2023-10-04 12:10:47 -0400

    update policies

---
+++ src/routes/[lang]/confidentialite/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+    throw redirect(302, 'https://zbranch.notion.site/Politique-de-confidentialit-6a82e05250564ef69d22ccd70d964152?pvs=4');
+}
