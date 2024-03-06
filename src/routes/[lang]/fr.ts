503461f6a59880c8ffe8a3845d47f5458616ee23
Etienne Beaudry Auger <850669+ebauger@users.noreply.github.com>
2024-03-06 08:47:00 -0500

    fix url calendly

--- src/routes/[lang]/fr.ts
+++ src/routes/[lang]/fr.ts
@@ -219,7 +219,7 @@ export const content = {
         "title": "Transformez votre vision en réalité",
         "subtitle": "Nous offrons une direction technique sans engagement total",
         "cta1": "Réserver un appel",
-        "cta1Href": "https://calendly.com/ebaexpert/allo-15",
+        "cta1Href": "https://calendly.com/zbranchio/allo-15",
         "cta2": "Commencer maintenant"
     }
 };
