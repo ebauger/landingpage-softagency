6157869801e29e19558d835c0b963e6dee262c93
Etienne Beaudry Auger <850669+ebauger@users.noreply.github.com>
2024-11-22 12:51:13 -0500

    feat: Add server-side redirect for privacy policy page

---
+++ src/routes/[lang]/privacy/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+    throw redirect(302, 'https://zbranch.notion.site/Privacy-Policy-1469184a339d80869114d82146ae849e');
+}
