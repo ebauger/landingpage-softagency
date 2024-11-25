59a43bb3148c81cc779568028b2d0bee44d47ac0
Etienne Beaudry Auger <850669+ebauger@users.noreply.github.com>
2024-11-25 14:39:27 -0500

    feat: Implement language-based redirection in page server load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,16 @@
 import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
+export const load: PageServerLoad = async ({ request }) => {
+    const acceptLanguage = request.headers.get('accept-language');
 
-export const load: LayoutServerLoad = () => {
+    if (acceptLanguage) {
+        const preferredLanguage = acceptLanguage.split(',')[0].split('-')[0];
+
+        if (preferredLanguage === 'en') {
+            throw redirect(302, '/en');
+        }
+    }
+
     throw redirect(302, '/fr');
 };
